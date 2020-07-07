8fdec1cf8a83395f62e27551262e8f2d80b5174e
codeFunny71 <whynotlaugh@gmail.com>
2020-07-07 11:59:39 -0700

    played around with golang print statements

--- 01_getting-started/01_helloWorld/main.go
+++ 01_getting-started/01_helloWorld/main.go
@@ -4,4 +4,26 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello world!")
+	foo()
+	fmt.Println("something more")
+
+	for i := 0; i < 100; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	}
+	bar()
 }
+
+func bar() {
+	fmt.Println("and then we exited")
+}
+
+func foo() {
+	fmt.Println("I'm in foo")
+}
+
+// control flow:
+// (1) sequence
+// (2) loop; iterative
+// (1) conditional
